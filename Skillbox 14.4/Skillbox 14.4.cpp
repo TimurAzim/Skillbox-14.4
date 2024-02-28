71ebfaf8b5b0f910f98593ee16a708ec4f77a5a6
Timur Azim <papaas73@gmail.com>
2024-02-28 17:17:54 +0600

    Skillbox 14.4

---
+++ Skillbox 14.4/Skillbox 14.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::string myString;
+    std::cout << "Введите ваше предложение: ";
+    std::getline(std::cin, myString);
+    std::cout << "Ваше предложение: " << myString << std::endl;
+    std::cout << "Длина строки: " << myString.length() << std::endl;
+    std::cout << "Первый символ строки: " << myString.front() << std::endl;
+    std::cout << "Последний символ строки: " << myString.back() << std::endl;
+
+    return 0;
+}
